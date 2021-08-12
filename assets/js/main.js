8e0a2a0608971597f9d57295d805014709e82ded
Ella Choy <egal21de@gmail.com>
2021-08-12 15:13:42 +0200

    Js_comparisons

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log("hi Comparision!");
+document.write("Übung lev1_15: Comparisions");
+
+// Verwende die unten aufgeführten Operatoren, um num mit anotherNum zu vergleichen.
+// Deklariere num = 5 und anotherNum = 8 als neue Variablen, dann gib die folgenden Vergleiche in der Konsole aus:
+
+let num=5;
+let anotherNum=8;
+console.log(num == anotherNum);// equal to  ==
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
